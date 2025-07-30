8cbc21b7c5a72044503bccc4d07fe7187eabb4bf
Lumi <drakko_games@outlook.com>
2025-07-30 18:25:17 -0300

    Update for TaskManager.py
    - Added a new function
    - Corrected some bugs while updating tasks.

--- Practice/TaskManager.py
+++ Practice/TaskManager.py
@@ -15,9 +15,16 @@ def visualiza_tarefa(tarefas):
         status = "✓" if tarefa["completada"] else "☓"
         nome_tarefa = tarefa["tarefa"] #Acessando a chave do dicionário tarefa tarefas.
         print(f"{indice} - [{status}] {nome_tarefa}")
+    return
 
-def atualiza_tarefa(tarefas):
-    
+
+def atualiza_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+    ajuste_indice = indice_tarefa - 1  #Tratando tamanho do indice da tarefa
+    if ajuste_indice >= 0 and ajuste_indice <= len(tarefas)
+    print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+    tarefas[indice_tarefa]["tarefa"] = novo_nome_tarefa
+    #A lista sempre começa no indice 0
+    return
     
 
 while True: 
@@ -30,7 +37,7 @@ while True:
         print("5 - Deletar tarefas completadas;")
         print("6 - Sair do programa.")
         opcao = [1, 2, 3, 4, 5, 6]
-        try: #Ao invés de fazer uma única variável, prefiro colocar uma lista como limitadora.
+        try:  #Ao invés de fazer uma única variável, prefiro colocar uma lista como limitadora.
             selecionaOpcao = int(input("Digite opcao desejada: \n"))
             if selecionaOpcao not in opcao:
                 print("Digite uma opcao válida!")
@@ -43,6 +50,15 @@ while True:
                 case 2:
                     print("Voce selecionou visualizar as tarefas!")
                     visualiza_tarefa(tarefas)
+                case 3:
+                    print("Voce selecionou editar tarefa!")
+                    visualiza_tarefa(tarefas) #Pro usuário ver quais tarefas estao na lista
+                    #Mas pra chamar a funçao atualiza_tarefa, precisamos definir duas variáveis
+                    #antes que estas possam ser utilizadas como argumentos da funçao
+                    indice_tarefa = int(input("indice da tarefa que deseja editar: "))
+                    novo_nome_tarefa = str(input("Novo nome da tarefa: "))
+                    #Penso em adicionar uma variável de informaçao da tarefa também
+                    atualiza_tarefa(tarefas, indice_tarefa, novo_nome_tarefa)
                 #O bloco try/except serve bem ao propósito.
         except ValueError:
             print("O valor digitado está incorreto!")
